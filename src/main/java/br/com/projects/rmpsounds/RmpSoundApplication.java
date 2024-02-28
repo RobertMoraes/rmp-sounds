1088fafc178b31bc542c395ec7129a2dcaeb1fc6
RobertMoraes <robert.moraes@outlook.com>
2024-02-28 18:50:47 -0300

    primeiro commit

---
+++ src/main/java/br/com/projects/rmpsounds/RmpSoundApplication.java
@@ -0,0 +1,13 @@
+package br.com.projects.rmpsounds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmpSoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmpSoundApplication.class, args);
+    }
+
+}
